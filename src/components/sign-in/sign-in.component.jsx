f67c0d31b7431e1380007860babede731592dcfc
JLLahti <38522273+JLLahti@users.noreply.github.com>
2020-07-21 21:12:06 +0300

    Added sign out button. Restyled sign in form component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -7,7 +7,7 @@ import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
-class SignIn extends React.Component{
+class SignIn extends React.Component {
     constructor(props) {
         super(props);
 
@@ -20,43 +20,45 @@ class SignIn extends React.Component{
     handleSubmit = event => {
         event.prevenDefault();
 
-        this.setState({ email: '', password: ''})
+        this.setState({ email: '', password: '' })
     }
 
     handleChange = event => {
         const { value, name } = event.target;
 
-        this.setState({ [name]: value})
+        this.setState({ [name]: value })
     }
 
     render() {
-        return(
+        return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                    name="email" 
-                    type="email" 
-                    handleChange={this.handleChange} 
-                    value={this.state.email} 
-                    label='email' required 
+                    <FormInput
+                        name="email"
+                        type="email"
+                        handleChange={this.handleChange}
+                        value={this.state.email}
+                        label='email' required
                     />
 
-                    <FormInput 
-                    name="password" 
-                    type="password" 
-                    value={this.state.password} 
-                    handleChange={this.handleChange} 
-                    label='password' required 
+                    <FormInput
+                        name="password"
+                        type="password"
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='password' required
                     />
-
-                    <CustomButton type='submit'>Sign in </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                        {' '}
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Sign in </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            {' '}
                         Sign in with Google {' '}
                         </CustomButton>
+                    </div>
+
                 </form>
             </div>
         );
