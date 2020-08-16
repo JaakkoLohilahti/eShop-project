1bd5045c0800cfe2ff5557fe7c2c0ee0d152555b
JLLahti <38522273+JLLahti@users.noreply.github.com>
2020-08-16 19:10:11 +0300

    changed shop theme to fitness store

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,16 +1,16 @@
 const INITIAL_STATE = {
     sections: [
         {
-        title: 'hats',
-        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+        title: 'cycling',
+        imageUrl: 'images/cycling.jpg',
         id: 1,
-        linkUrl: 'shop/hats'
+        linkUrl: 'shop/cycling'
       },
       {
-        title: 'jackets',
-        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+        title: 'supplements',
+        imageUrl: 'images/supplements.jpg',
         id: 2,
-        linkUrl: 'shop/jackets'
+        linkUrl: 'shop/supplements'
       },
       {
         title: 'sneakers',
@@ -19,18 +19,18 @@ const INITIAL_STATE = {
         linkUrl: 'shop/sneakers'
       },
       {
-        title: 'womens',
-        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        title: 'weights',
+        imageUrl: 'images/weights.jpg',
         size: 'large',
         id: 4,
-        linkUrl: 'shop/womens'
+        linkUrl: 'shop/weights'
       },
       {
-        title: 'mens',
-        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        title: 'clothing',
+        imageUrl: 'images/clothes.JPG',
         size: 'large',
         id: 5,
-        linkUrl: 'shop/mens'
+        linkUrl: 'shop/clothing'
       }
     ]
 };
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,62 +1,50 @@
 const SHOP_DATA = {
-  hats: {
+  cycling: {
     id: 1,
-    title: 'Hats',
-    routeName: 'hats',
+    title: 'Cycling',
+    routeName: 'cycling',
     items: [
       {
         id: 1,
-        name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+        name: 'Black Bike',
+        imageUrl: '/images/shop-img/Cycling/blackBike.JPG',
         price: 25
       },
       {
         id: 2,
-        name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-        price: 18
+        name: 'Black Helmet',
+        imageUrl: '/images/shop-img/Cycling/BlackHelmet.JPG',
+        price: 20
       },
       {
         id: 3,
-        name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        price: 35
+        name: 'Road Bike',
+        imageUrl: '/images/shop-img/Cycling/blackroadbike.JPG',
+        price: 80
       },
       {
         id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        price: 25
+        name: 'Green Road Bike',
+        imageUrl: '/images/shop-img/Cycling/greenroadbike.JPG',
+        price: 80
       },
       {
         id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        price: 18
+        name: 'Red Bike',
+        imageUrl: '/images/shop-img/Cycling/redBike.JPG',
+        price: 45
       },
       {
         id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-        price: 14
+        name: 'City Bike',
+        imageUrl: '/images/shop-img/Cycling/yellowcitybike.JPG',
+        price: 135
       },
       {
         id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-        price: 18
-      },
-      {
-        id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-        price: 14
-      },
-      {
-        id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-        price: 16
+        name: 'Vintage Bike',
+        imageUrl: '/images/shop-img/Cycling/blackvintagebike.JPG',
+        price: 20
       }
     ]
   },
@@ -66,180 +54,162 @@ const SHOP_DATA = {
     routeName: 'sneakers',
     items: [
       {
-        id: 10,
+        id: 8,
         name: 'Adidas NMD',
         imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
         price: 220
       },
       {
-        id: 11,
+        id: 9,
         name: 'Adidas Yeezy',
         imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
         price: 280
       },
       {
-        id: 12,
+        id: 10,
         name: 'Black Converse',
         imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
         price: 110
       },
       {
-        id: 13,
+        id: 11,
         name: 'Nike White AirForce',
         imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
         price: 160
       },
       {
-        id: 14,
+        id: 12,
         name: 'Nike Red High Tops',
         imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
         price: 160
       },
       {
-        id: 15,
+        id: 13,
         name: 'Nike Brown High Tops',
         imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
         price: 160
       },
       {
-        id: 16,
+        id: 14,
         name: 'Air Jordan Limited',
         imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
         price: 190
       },
       {
-        id: 17,
+        id: 15,
         name: 'Timberlands',
         imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
         price: 200
       }
     ]
   },
-  jackets: {
+  supplements: {
     id: 3,
-    title: 'Jackets',
-    routeName: 'jackets',
+    title: 'Supplements',
+    routeName: 'supplements',
     items: [
       {
-        id: 18,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+        id: 16,
+        name: 'GH MAX',
+        imageUrl: '/images/shop-img/supplements/GHMAX.JPG',
         price: 125
       },
       {
-        id: 19,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+        id: 17,
+        name: 'Hemp Oil',
+        imageUrl: '/images/shop-img/supplements/hempOil.JPG',
         price: 90
       },
       {
-        id: 20,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+        id: 18,
+        name: 'Omega-3 Capsules',
+        imageUrl: '/images/shop-img/supplements/omega3.jpg',
         price: 90
+      },
+      {
+        id: 19,
+        name: 'Whey Protein',
+        imageUrl: '/images/shop-img/supplements/protein.JPG',
+        price: 165
+      }
+    ]
+  },
+  weights: {
+    id: 4,
+    title: 'Weights',
+    routeName: 'weights',
+    items: [
+      {
+        id: 20,
+        name: '2kg Dumbbell',
+        imageUrl: '/images/shop-img/weights/2kgdumbbell.jpg',
+        price: 25
       },
       {
         id: 21,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        price: 165
+        name: '10kg Dumbbell',
+        imageUrl: '/images/shop-img/weights/10kgdumbbell.jpg',
+        price: 20
       },
       {
         id: 22,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        price: 185
-      }
-    ]
-  },
-  womens: {
-    id: 4,
-    title: 'Womens',
-    routeName: 'womens',
-    items: [
+        name: '20kg Kettlebell',
+        imageUrl: '/images/shop-img/weights/20kgkettlebell.jpg',
+        price: 80
+      },
       {
         id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-        price: 25
+        name: 'Dumbbell Set',
+        imageUrl: '/images/shop-img/weights/dumbbellset.jpg',
+        price: 80
       },
       {
         id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20
-      },
-      {
-        id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80
-      },
-      {
-        id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80
-      },
-      {
-        id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
+        name: 'EZ bar',
+        imageUrl: '/images/shop-img/weights/ezbar.jpg',
         price: 45
       },
       {
-        id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
+        id: 25,
+        name: 'Kettlebell Set',
+        imageUrl: '/images/shop-img/weights/kettlebellSet.jpg',
         price: 135
       },
       {
-        id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
+        id: 26,
+        name: 'Weight Set',
+        imageUrl: '/images/shop-img/weights/weightSet.jpg',
         price: 20
       }
     ]
   },
-  mens: {
+  clothing: {
     id: 5,
-    title: 'Mens',
-    routeName: 'mens',
+    title: 'Clothing',
+    routeName: 'clothing',
     items: [
       {
-        id: 30,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+        id: 27,
+        name: 'Mens Hoodie',
+        imageUrl: '/images/shop-img/clothing/menshoodie.JPG',
         price: 325
       },
       {
-        id: 31,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+        id: 28,
+        name: 'Sleeveless Hoodie',
+        imageUrl: '/images/shop-img/clothing/sleevelesshoodie.JPG',
         price: 20
       },
       {
-        id: 32,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+        id: 29,
+        name: 'Womens Tank Top',
+        imageUrl: '/images/shop-img/clothing/womentanktop.JPG',
         price: 25
       },
       {
-        id: 33,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        price: 25
-      },
-      {
-        id: 34,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40
-      },
-      {
-        id: 35,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+        id: 30,
+        name: 'Womens Top',
+        imageUrl: '/images/shop-img/clothing/womentop.JPG',
         price: 25
       }
     ]
